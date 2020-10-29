6816f70e75399835bf229ffea7da91c355cc8d5b
sigHound <snp3rk@gmail.com>
2020-10-29 16:30:55 -0500

    Update 1.1.cpp

--- 1.1.cpp
+++ 1.1.cpp
@@ -1,20 +1,18 @@
-//1.2
-
 #include <iostream>
 
 int main(){
-    int myArraySize = 5;
-    int myArray[] = {1,2,3,4,5};
-    int newArray[myArraySize];
-    for (int i = 0; i < myArraySize; i++){
+    int CONST SIZE;
+    int myArray[SIZE];
+    int newArray[SIZE];
+    for (int i = 0; i < SIZE; i++){
         newArray[i] = 1;
-        for (int j = 0; j < myArraySize; j++){
+        for (int j = 0; j < SIZE; j++){
             if(i != j){
                 newArray[i] *= myArray[j];
             }
         }
     }
-    for (int i = 0; i < myArraySize; i++){
+    for (int i = 0; i < SIZE; i++){
         std::cout << newArray[i] << std::endl;
     }
 }
